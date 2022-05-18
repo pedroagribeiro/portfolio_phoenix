97b5361acdaaad110c67b3166ecd36b8d08f537a
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-05-18 21:09:59 +0100

    :art: Add Tailwind CSS support to the project

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,8 @@ config :portfolio, PortfolioWeb.Endpoint,
   secret_key_base: "K6gZcEnrldxPmyAwWv4oXuPsF0te2Yndrvmkl2xKLmJ9ILp10Cba0VhJuFeo9cLb",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
